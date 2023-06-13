aacf395dcf6ad9b5156f0487d518cbfab620e0d2
Evan Bauer <18405113+evbauer@users.noreply.github.com>
2023-06-13 11:21:17 -0400

    update for May 2023 inflation report

--- plot.py
+++ plot.py
@@ -86,7 +86,7 @@ ymax = 87000
 ymin = 73000
 ax2 = ax[1].twinx()
 ax2.set_ylim(ymin,ymax)
-ax2.set_ylabel('Stipend in Real Dollars (April 2023)')
+ax2.set_ylabel('Stipend in Real Dollars (May 2023)')
 ax[1].grid(False,axis='y')
 ax[1].set_ylabel('Stipend in 2011 Dollars')
 ax[1].set_ylim(ymin*cpi[0]/cpi[-1],ymax*cpi[0]/cpi[-1])
