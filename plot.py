47401a49f75b48921276559d5cc7470950a16397
evbauer <18405113+evbauer@users.noreply.github.com>
2024-03-02 10:41:12 -0500

    update for January CPI report

--- plot.py
+++ plot.py
@@ -87,7 +87,7 @@ ymax = 89000
 ymin = 74000
 ax2 = ax[1].twinx()
 ax2.set_ylim(ymin,ymax)
-ax2.set_ylabel('Stipend in Real Dollars (December 2023)')
+ax2.set_ylabel('Stipend in Real Dollars (January 2024)')
 ax[1].grid(False,axis='y')
 ax[1].set_ylabel('Stipend in 2011 Dollars')
 ax[1].set_ylim(ymin*cpi[0]/cpi[-1],ymax*cpi[0]/cpi[-1])
